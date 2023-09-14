fbd03ec901ce8106a1b4a796da61891ce94d8108
bamjam101 <herejamil123@gmail.com>
2023-09-14 18:35:36 +0530

    About page: fixed the cards layout.

--- src/components/Pages/About/about.jsx
+++ src/components/Pages/About/about.jsx
@@ -8,82 +8,78 @@ const Head = AboutData.filter((EventsData) => EventsData.category === "head");
 const Assit = AboutData.filter((EventsData) => EventsData.category === "asst");
 
 function CreateCard(Members) {
-	return (
-		<AboutUsCard
-			name={Members.name}
-			post={Members.post}
-			key={Members.id}
-			instagram={Members.instagram}
-			linkdin={Members.linkdin}
-			img={Members.img}
-		/>
-	);
+  return (
+    <AboutUsCard
+      name={Members.name}
+      post={Members.post}
+      key={Members.id}
+      instagram={Members.instagram}
+      linkdin={Members.linkdin}
+      img={Members.img}
+    />
+  );
 }
 
 function TopCards() {
-	return (
-		<>
-			<div className="ourTeamBlock">{Top.map(CreateCard)}</div>
-		</>
-	);
+  return (
+    <>
+      <div className="ourTeamBlock">{Top.map(CreateCard)}</div>
+    </>
+  );
 }
 
 function HeadCards() {
-	return (
-		<>
-			<div className="ourTeamBlock">{Head.map(CreateCard)}</div>
-		</>
-	);
+  return (
+    <>
+      <div className="ourTeamBlock">{Head.map(CreateCard)}</div>
+    </>
+  );
 }
 
 function AssitCards() {
-	return (
-		<>
-			<div className="ourTeamBlock">{Assit.map(CreateCard)}</div>
-		</>
-	);
+  return (
+    <>
+      <div className="ourTeamBlock">{Assit.map(CreateCard)}</div>
+    </>
+  );
 }
 
 export default function About() {
-	return (
-		<>
-			<div className="about-ieee">
-				<div className="boxone">
-					<img
-						src="IEEE_Atharva_logo_-removebg.png"
-						className="iee"
-					></img>
-					<img src="logo.png" className="alca"></img>
-					<img src="ACE.png" className="ace"></img>
-					<img
-						src="background.jpg"
-						alt="poster"
-						className="bgimg"
-					></img>
-				</div>
-				<div className="boxtwo">
-					<h1>ABOUT IEEE TECHITHON</h1>
-					<p>
-						The Atharva College Of Engineering's Annual Technical
-						Fest - IEEE TECHITHON, promotes engineering undergrads
-						to demonstrate their technical prowess and develop new
-						innovations. The major goal is to cultivate a drive for
-						technical proficiency, logical cognition, logical
-						reasoning and intelligent decision-making.A city-wide
-						platform for all Technology Fans to display their skills
-						and produce amazing products. IEEE TECHITHON '22 is a
-						culmination of the work done by Young Tech Wizards.
-					</p>
-				</div>
-			</div>
-			<div className="gridDisplay">
-				<h1 className="title">Our Team</h1>
-				<TopCards />
-				<h1 className="head">Heads</h1>
-				<HeadCards />
-				<h1 className="ah">Assitant Heads</h1>
-				<AssitCards />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="about-ieee">
+        <div className="boxone">
+          <img
+            src="IEEE_Atharva_logo_-removebg.png"
+            className="iee"
+            alt="IEEE-Atharva-Logo"
+          ></img>
+          <img src="logo.png" alt="Logo" className="alca"></img>
+          <img src="ACE.png" alt="Atharva-Logo" className="ace"></img>
+          <img src="background.jpg" alt="poster" className="bgimg"></img>
+        </div>
+        <div className="boxtwo">
+          <h1>ABOUT IEEE TECHITHON</h1>
+          <p>
+            The Atharva College Of Engineering's Annual Technical Fest - IEEE
+            TECHITHON, promotes engineering undergrads to demonstrate their
+            technical prowess and develop new innovations. The major goal is to
+            cultivate a drive for technical proficiency, logical cognition,
+            logical reasoning and intelligent decision-making.A city-wide
+            platform for all Technology Fans to display their skills and produce
+            amazing products. IEEE TECHITHON '22 is a culmination of the work
+            done by Young Tech Wizards.
+          </p>
+        </div>
+      </div>
+      <div className="gridDisplay">
+        <h1 className="title">Our Team</h1>
+        <TopCards />
+        <h1 className="head">Heads</h1>
+        <HeadCards />
+        <h1 className="ah">Assitant Heads</h1>
+        <AssitCards />
+      </div>
+    </>
+  );
 }
--- src/components/Ticket/index.jsx
+++ src/components/Ticket/index.jsx
@@ -4,54 +4,55 @@ import axios from "axios";
 import config from "../../config";
 
 function Ticket() {
-	const [user, setUser] = useState();
-	const [events, setEvents] = useState([]);
+  const [user, setUser] = useState();
+  const [events, setEvents] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get(`${config.backendLocation}/user/self`, {
-				headers: { token: localStorage.token },
-			})
-			.then((res) => {
-				setUser(res.data);
-				// setInterval(() => {
-				// 	window.print();
-				// 	window.location = "/";
-				// }, 3000);
-			});
+  useEffect(() => {
+    axios
+      .get(`${config.backendLocation}/user/self`, {
+        headers: { token: localStorage.token },
+      })
+      .then((res) => {
+        setUser(res.data);
+        // setInterval(() => {
+        // 	window.print();
+        // 	window.location = "/";
+        // }, 3000);
+      });
 
-		axios
-			.get(`${config.backendLocation}/register`, {
-				headers: { token: localStorage.token },
-			})
-			.then((res) => {
-				setEvents(res.data);
-			});
-	}, []);
+    axios
+      .get(`${config.backendLocation}/register`, {
+        headers: { token: localStorage.token },
+      })
+      .then((res) => {
+        setEvents(res.data);
+      });
+  }, []);
 
-	return (
-		<>
-			{user && (
-				<div className="ticket" id="ticket">
-					Name: {user.username}
-					<br />
-					Email: {user.email}
-					<br />
-					<img
-						className="qr"
-						src={`https://api.qrserver.com/v1/create-qr-code/?size=600x600&data=${user._id}`}
-					/>
-					<br />
-					Events:
-					<ul>
-						{events.map((event, i) => (
-							<li key={i}>{event.name}</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      {user && (
+        <div className="ticket" id="ticket">
+          Name: {user.username}
+          <br />
+          Email: {user.email}
+          <br />
+          <img
+            className="qr"
+            alt="QR-Code"
+            src={`https://api.qrserver.com/v1/create-qr-code/?size=600x600&data=${user._id}`}
+          />
+          <br />
+          Events:
+          <ul>
+            {events.map((event, i) => (
+              <li key={i}>{event.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Ticket;
