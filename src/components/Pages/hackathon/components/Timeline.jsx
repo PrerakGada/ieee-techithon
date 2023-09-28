77b973753a512145e03bf6ee64cdadd2b9c269b4
“Akash <kolakkalakash@gmail.com>
2023-09-29 02:07:17 +0530

    added timeline section

--- src/components/Pages/hackathon/components/Timeline.jsx
+++ src/components/Pages/hackathon/components/Timeline.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "../styles/sponsers.module.css";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div className={styles.section}>
+      <div className={styles.justify}>
+        <svg className={styles.linesvg} xmlns="http://www.w3.org/2000/svg" width="93" height="6" viewBox="0 0 93 6" fill="none">
+          <path d="M-0.0107756 3L92.127 3" stroke="#FFF85A" stroke-width="5" />
+        </svg>
+        <div className={styles.heading}>
+          Timeline
+        </div>
+        <svg className={styles.linesvg} xmlns="http://www.w3.org/2000/svg" width="93" height="6" viewBox="0 0 93 6" fill="none">
+          <path d="M-0.0107756 3L92.127 3" stroke="#FFF85A" stroke-width="5" />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
