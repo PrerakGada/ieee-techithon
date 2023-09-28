905f72507b1a636ce1981635ce029666569721cf
“Akash <kolakkalakash@gmail.com>
2023-09-29 02:19:29 +0530

    minor alignment changes of section titles

--- src/components/Pages/hackathon/components/Faqs.jsx
+++ src/components/Pages/hackathon/components/Faqs.jsx
@@ -45,11 +45,15 @@ const FAQs = () => {
     <>
     <div className={styles.section}>
       <div className={styles.justify}>
-       <svg className={styles.linesvg} xmlns="http://www.w3.org/2000/svg" width="93" height="6" viewBox="0 0 93 6" fill="none">
-        <path d="M-0.0107756 3L92.127 3" stroke="#FFF85A" stroke-width="5" />
-      </svg>
-
-      <p className={styles.heading}>FAQ's</p>
+      <svg className={styles.linesvg} xmlns="http://www.w3.org/2000/svg" width="93" height="6" viewBox="0 0 93 6" fill="none">
+          <path d="M-0.0107756 3L92.127 3" stroke="#FFF85A" stroke-width="5" />
+        </svg>
+        <div className={styles.heading}>
+          FAQ's
+        </div>
+        <svg className={styles.linesvg} xmlns="http://www.w3.org/2000/svg" width="93" height="6" viewBox="0 0 93 6" fill="none">
+          <path d="M-0.0107756 3L92.127 3" stroke="#FFF85A" stroke-width="5" />
+        </svg>
       </div>
 {/*
       <div className={styles.cells}>
