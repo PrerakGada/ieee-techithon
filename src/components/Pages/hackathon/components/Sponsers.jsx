0abb6c2c6a23bb38ece267fbc811a67aec9e0404
“Akash <kolakkalakash@gmail.com>
2023-09-29 21:11:20 +0530

    added sponser cards

--- src/components/Pages/hackathon/components/Sponsers.jsx
+++ src/components/Pages/hackathon/components/Sponsers.jsx
@@ -28,6 +28,52 @@ const Sponsers = () => {
             <path d="M-0.0107756 3L92.127 3" stroke="#FFF85A" strokeWidth="5" />
           </svg>
         </div>
+        <div className={styles.grid}>
+          <div className={styles.sponser}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="350" viewBox="0 0 155 449" fill="none">
+              <g filter="url(#filter0_f_41_84)">
+                <path d="M4 24C4 12.9543 12.9543 4 24 4H131C142.046 4 151 12.9543 151 24V425C151 436.046 142.046 445 131 445H24C12.9543 445 4 436.046 4 425V24Z" fill="#FDFAB5" fill-opacity="0.1" />
+              </g>
+              <defs>
+                <filter id="filter0_f_41_84" x="0" y="0" width="155" height="449" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                  <feGaussianBlur stdDeviation="2" result="effect1_foregroundBlur_41_84" />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+          <div className={styles.sponser}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="350" viewBox="0 0 155 449" fill="none">
+              <g filter="url(#filter0_f_41_84)">
+                <path d="M4 24C4 12.9543 12.9543 4 24 4H131C142.046 4 151 12.9543 151 24V425C151 436.046 142.046 445 131 445H24C12.9543 445 4 436.046 4 425V24Z" fill="#FDFAB5" fill-opacity="0.1" />
+              </g>
+              <defs>
+                <filter id="filter0_f_41_84" x="0" y="0" width="155" height="449" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                  <feGaussianBlur stdDeviation="2" result="effect1_foregroundBlur_41_84" />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+          <div className={styles.sponser}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="350" viewBox="0 0 155 449" fill="none">
+              <g filter="url(#filter0_f_41_84)">
+                <path d="M4 24C4 12.9543 12.9543 4 24 4H131C142.046 4 151 12.9543 151 24V425C151 436.046 142.046 445 131 445H24C12.9543 445 4 436.046 4 425V24Z" fill="#FDFAB5" fill-opacity="0.1" />
+              </g>
+              <defs>
+                <filter id="filter0_f_41_84" x="0" y="0" width="155" height="449" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                  <feGaussianBlur stdDeviation="2" result="effect1_foregroundBlur_41_84" />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+
+        </div>
+
       </div>
       <div className={styles.section}>
         <div className={styles.justify}>
