cfcd6c2bf335fccd2a1bec281a68c13d7625828b
“Akash <kolakkalakash@gmail.com>
2023-10-01 12:48:38 +0530

    added devfolio button

--- src/components/Pages/hackathon/components/Hero.jsx
+++ src/components/Pages/hackathon/components/Hero.jsx
@@ -1,6 +1,20 @@
 import styles from "../styles/hero.module.css";
+import React from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+
   return (
     <section className={styles.container}>
       {/* <img
@@ -84,12 +98,11 @@ const Hero = () => {
             </div>
             <div
               className="apply-button"
-              data-hackathon-slug="wmn2023"
+              data-hackathon-slug="hackmania"
               data-button-theme="light"
-              style={{ height: "44px", width: "312px" }}
-            >
-              Apply via Devfolio
-            </div>
+              style={{ height: '44px', width: '312px' }}
+            ></div>
+
           </div>
         </section>
       </main>
